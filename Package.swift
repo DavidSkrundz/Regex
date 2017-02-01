e8a02a1cfced3092f884159e3fe0111b93bef245
David Skrundz <david@skrundz.ca>
2017-02-01 12:54:15 -0700

    Change dependency information

--- Package.swift
+++ Package.swift
@@ -8,8 +8,8 @@ import PackageDescription
 let package = Package(
 	name: "Regex",
 	dependencies: [
-		.Package(url: "https://github.com/DavidSkrundz/UnicodeOperators.git", versions: Version(1,0,0)..<Version(1,1,0)),
-		.Package(url: "https://github.com/DavidSkrundz/Util.git", versions: Version(1,0,0)..<Version(1,1,0)),
+		.Package(url: "https://github.com/DavidSkrundz/UnicodeOperators.git", majorVersion: 1, minor: 0),
+		.Package(url: "https://github.com/DavidSkrundz/Util.git", majorVersion: 1, minor: 0),
 	]
 )
 
