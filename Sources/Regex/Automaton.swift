b10bc0631ef5a7533b581e39bb3eaf85539b9d66
David Skrundz <david@skrundz.ca>
2018-06-07 00:48:20 -0600

    Add functions to calculate epsilon-closures

--- Sources/Regex/Automaton.swift
+++ Sources/Regex/Automaton.swift
@@ -23,4 +23,23 @@ internal class Automaton {
 	internal func addTransition(from: State, to: State, symbol: Symbol) {
 		self.transitions[from, default: [:]][symbol, default: []].insert(to)
 	}
+	
+	internal func ε_closure(of state: State) -> Set<State> {
+		var stack = [state]
+		var closure = Set<State>()
+		while !stack.isEmpty {
+			let s = stack.removeFirst()
+			closure.insert(s)
+			if let newStates = self.transitions[s]?[.None] {
+				stack.append(contentsOf: newStates)
+			}
+		}
+		return closure
+	}
+	
+	internal func ε_closure(of states: Set<State>) -> Set<State> {
+		return states
+			.map { self.ε_closure(of: $0) }
+			.reduce(Set<State>()) { $0.union($1) }
+	}
 }
